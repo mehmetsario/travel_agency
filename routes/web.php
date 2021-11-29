5cd7f348ab9c05301ea1381069436295fb53080a
mehmetsario <mohamad.sario@ogr.iuc.edu.tr>
2021-11-29 13:38:24 +0300

    Adding files

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 
 Route::get('/', function () {
     return view('index');
